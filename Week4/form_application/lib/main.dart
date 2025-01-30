c18b592c5c1731909a9d8587337094f657bff332
Tom Maurer <tmaurer@nait.ca>
2025-01-30 10:21:01 -0700

    week 4 day 3 stage 4

--- Week4/form_application/lib/main.dart
+++ Week4/form_application/lib/main.dart
@@ -31,6 +31,19 @@ class _UserSignupFormState extends State<UserSignupForm> {
 
   final _formKey = GlobalKey<FormState>();
 
+  // We need controllers to get information out of the fields
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controllers when the widget is unmounted
+    _usernameController.dispose();
+    _passwordController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -39,6 +52,8 @@ class _UserSignupFormState extends State<UserSignupForm> {
         children: [
           Text('New User Form'),
           TextFormField(
+            // Bind the appropriate controller
+            controller: _usernameController,
             validator: (value) {
               return value == null || value.isEmpty
                   ? 'Username cannot be empty'
@@ -49,6 +64,7 @@ class _UserSignupFormState extends State<UserSignupForm> {
             ),
           ),
           TextFormField(
+            controller: _passwordController,
             obscureText: true,
             enableSuggestions: false,
             autocorrect: false,
@@ -59,12 +75,14 @@ class _UserSignupFormState extends State<UserSignupForm> {
           ElevatedButton(
             onPressed: () {
               if (_formKey.currentState!.validate()) {
-                _formKey.currentState!.reset();
                 // This is where we would get the data and send it off to our backend
-
                 ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('processing...')),
+                  SnackBar(
+                      content: Text(
+                          'Username: ${_usernameController.text}, Password: ${_passwordController.text}')),
                 );
+
+                _formKey.currentState!.reset();
               }
             },
             child: Text('Sign up'),
